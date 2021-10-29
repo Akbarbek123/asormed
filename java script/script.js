9fb6c4fb9e5324a16eac4911cabd2f306b8a7ee5
Akbarbek123 <akbarbekahtamov123@gmail.com>
2021-10-29 15:43:28 +0500

    loader

---
+++ java script/script.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded',function(){
+      const loader=document.querySelector('.loader');
+      setTimeout(function () {
+            loader.style.display="none";
+            loader.style.opacity=0;
+      },2000)
+})
